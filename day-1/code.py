d25125c4af9e3980e8cd8f5d3ed70179488850a6
Divyanshu Giri <divyanshugiri007@gmail.com>
2025-12-18 23:04:00 +0530

    day-1

---
+++ day-1/code.py
@@ -0,0 +1,32 @@
+
+def lock_picker( combo ):
+
+    pos = 50
+    combo_list = combo
+    count = 0
+
+    for each in combo_list:
+
+        if(each[0] == 'R'):
+            pos += int(each[1:])
+        elif (each[0] == 'L'):
+            pos -= int(each[1:])
+
+        while( pos > 99):
+            pos -= 100
+        while (pos < 0):
+            pos += 100
+
+        if(pos == 0):
+            count += 1
+        
+        # print(pos)
+
+    return count
+
+# data = input("enter:")\
+
+with open("input.txt", "r", encoding="utf-8") as file:
+    data = file.readlines()
+result = lock_picker(data)
+print(result)
